842339b8dbc3004ba5e4a37959486109e096caf2
Krystal Foster and Kate Goss <krystal@>
2018-08-21 16:48:50 -0700

    troubleshoot

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,64 @@
-// function BankAccount(name, balance) {
-//   this.name = name;
-//   this.balance = balance;
-// }
-//
-//
-// BankAccount.prototype.deposit = function(amount) {
-//   this.balance += amount;
-// }
-//
-// Account.prototype.withdrawal = function() {
-//   this.balance -= amount;
-// }
+function BankAccount(name, balance) {
+  this.name = name;
+  this.balance = balance;
+}
 
+function Deposit(deposit) {
+  this.deposit = deposit;
+}
+
+function resetFields() {
+  $("input#first-deposit").val("");
+  $("input#deposit").val("");
+  $("input#withdrawal").val("");
+};
+
+BankAccount.prototype.deposit = function() {
+  return this.balance += this.deposit;
+}
+
+// BankAccount.prototype.withdrawal = function(additionalWithdrawal) {
+//   this.balance -= additionalWithdrawal;
+// }
 
 $(document).ready(function() {
-  var amount = $("#formOne").submit(function() {
+   $("#formOne").submit(function() {
     event.preventDefault();
     var inputtedName = $("input#name").val();
     var inputtedFirstDeposit = parseInt($("input#first-deposit").val());
 
-    // var amount = new BankAccount(name, balance)
+    var newBankAccount = new BankAccount(inputtedName, inputtedFirstDeposit);
 
     $(".display").text(inputtedFirstDeposit);
     $("#first-well").show();
   });
 
-    $("#formTwo").submit(function() {
+   $("#formTwo").submit(function() {
       event.preventDefault();
       var inputtedDeposit = parseInt($("input#deposit").val());
-      $(".display-deposit").text(inputtedDeposit);
+
+      var newDeposit = new Deposit(inputtedFirstDeposit, inputtedDeposit)
+
+      $(".display-deposit").text(newBankAccount.newDeposit);
       $("#deposit-well").show();
-
     });
 
-    $("#formThree").submit(function() {
-      event.preventDefault();
-      var inputtedWithdrawal = parseInt($("input#withdrawal").val());
-      $(".display-withdrawal").text(inputtedWithdrawal);
-      $("#withdrawal-well").show();
-
-    });
-
+  // var additionalWithdrawal =  $("#formThree").submit(function() {
+  //     event.preventDefault();
+  //     var inputtedWithdrawal = parseInt($("input#withdrawal").val());
+  //     $(".display-withdrawal").text(inputtedWithdrawal);
+  //     $("#withdrawal-well").show();
+  //
+  //   });
 
 
   // $("#deposit").click(function() {
-  //   BankAccount.deposit(amount)
+  //   BankAccount.deposit(additionalDeposit)
   // });
   // $("#withdrawal").click(function() {
   //   BankAccount.withdrawal(amount)
   // });
+    function resetFields(){
+
+    };
 });
